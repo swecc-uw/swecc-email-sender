1fd5a4c5b4b6384a75f955a0dd5209e7eab331ce
Elijah Melton <elimelt@uw.edu>
2025-02-27 21:52:10 -0800

    bump version

--- swecc_email_sender/__init__.py
+++ swecc_email_sender/__init__.py
@@ -5,5 +5,5 @@ SWECC Email Sender - An email automation library using SendGrid.
 from swecc_email_sender.core.loader import DataLoader
 from swecc_email_sender.core.sender import EmailSender
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __all__ = ["DataLoader", "EmailSender"]
