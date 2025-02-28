e3e91d0ccd6a39eafb876c748e7d75ae32855be3
Elijah Melton <elimelt@uw.edu>
2025-02-27 22:29:59 -0800

    strip inputs

--- swecc_email_sender/__init__.py
+++ swecc_email_sender/__init__.py
@@ -5,5 +5,5 @@ SWECC Email Sender - An email automation library using SendGrid.
 from swecc_email_sender.core.loader import DataLoader
 from swecc_email_sender.core.sender import EmailSender
 
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 __all__ = ["DataLoader", "EmailSender"]
